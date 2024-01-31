6deb5dbf89565aee9bed7c4ce9d53017866bf979
TaeHo Kim <xoghtjddus@gmail.com>
2024-01-31 09:03:10 +0900

    2024-01-30(2)

--- javascript test/javascript/event.js
+++ javascript test/javascript/event.js
@@ -1,16 +1,103 @@
-
 let p;
 
-function init(){
+function init() {
   p = document.getElementById("p");
   p.addEventListener("mouseover", over);
-  p.addEventListener("mouseout" , out);
-  
+  p.addEventListener("mouseout", out);
 }
 
-function over(){
+function over() {
   p.style.backgroundColor = "violet";
 }
-function out(){
+function out() {
   p.style.backgroundColor = "green";
+}
+
+function init2() {
+  p = document.getElementById("p");
+  p.onmouseover = function () {
+    this.style.backgroundColor = "violet";
+  };
+  p.onmouseout = function () {
+    this.style.backgroundColor = "green";
+  };
+}
+
+function f(e) {
+  alert(e.type);
+}
+
+// document.getElementById("j").onmouseover = f;
+
+function run(e) {
+  let text =
+    "type:" +
+    e.type +
+    "<br>" +
+    "target" +
+    e.target +
+    "<br>" +
+    "currentTarget:" +
+    e.currentTarget +
+    "<br>" +
+    "defaltPrevented";
+
+  let g = document.getElementById("g");
+  g.innerHTML = text;
+}
+
+function cal() {
+  let exp = document.getElementById("exp");
+  let result = document.getElementById("result");
+  result.value = eval(exp.value);
+}
+
+let width = 1;
+
+function down(obj) {
+  obj.style.fontStyle = "italic";
+}
+
+function up(obj) {
+  obj.style.fontStyle = "normal";
+}
+
+function over(obj) {
+  obj.style.borderColor = "violet";
+}
+
+function out(obj) {
+  obj.style.borderColor = "gray";
+}
+
+function wheel(e, obj) {
+  if (e.wheelDelta < 0) {
+    width--;
+    if (width < 0) {
+      width = 0;
+    } 
+  } else {
+    width++;
+  }
+  obj.style.borderStyle = "ridge";
+  obj.style.borderWidth = width + "px";
+}
+
+
+function hide(){
+  alert("No");
+  return false;
+}
+
+document.oncontextmenu = hide;
+
+function image(){
+  let px = document.getElementById("px");
+  let img = document.getElementById("img");
+  img.onload = function(){
+    let mySpan = document.getElementById("mySpan");
+    mySpan.innerHTML = img.width + X + img.height;
+  }
+  let index = px.selectedIndex;
+  img.src = px.options[index].value;
 }
--- javascript test/javascript/my.js
+++ javascript test/javascript/my.js
@@ -1,8 +1,8 @@
 let i = 0;
 function talking() {
   let talks = [
-    { tall: "안녕!!" },
-    { tall: "반가워ㅎㅎ" },
+    { tall: "안녕!!", img: "../img/어른때.jpg" },
+    { tall: "반가워ㅎㅎ" , img: "../img/어릴때.jpg"},
     { tall: "뭐하고있어?" },
     { tall: "나?" },
     { tall: "나는 유튜브 보고있었지" },
@@ -19,18 +19,30 @@ function talking() {
   let a = talks[i];
   const jsona = JSON.stringify(a);
   let obj = document.getElementById("now");
+  let imgpapa = document.getElementById("imgNow");
   let newTalk = document.createElement("div");
   newTalk.innerHTML = a.tall;
   newTalk.setAttribute("id", "myTalk");
   newTalk.style.fontSize = "30px";
   newTalk.style.textAlign = "center";
-  alert(i);
+
+  // newImg.innerHTML = a.img;
+  let newImg = document.createElement("img");
+  newImg.setAttribute("id","myImg");
+  newImg.style.width = "300px";
+  newImg.style.height = "200px";
+  // let y = talks.img[i]
+  newImg.src = a.img;
+
   if (i <= talks.length && i > 0) {
+    imgpapa.removeChild(myImg);
     obj.removeChild(myTalk);
+    imgpapa.appendChild(newImg);
     obj.appendChild(newTalk);
     i++;
   } else if(i == 0 || i < 0){
     i = 0;
+    imgpapa.appendChild(newImg);
     obj.appendChild(newTalk);
     i++;
   }else{
@@ -41,9 +53,9 @@ function talking() {
 console.log(i);
 function removeTalking() {
   let talks = [
-    { tall: "안녕!!" },
-    { tall: "안녕!!" },
-    { tall: "반가워ㅎㅎ" },
+    { tall: "안녕!!", img: "../img/어른때.jpg"},
+    { tall: "안녕!!", img: "../img/어른때.jpg" },
+    { tall: "반가워ㅎㅎ" , img: "../img/어릴때.jpg"},
     { tall: "뭐하고있어?" },
     { tall: "나?" },
     { tall: "나는 유튜브 보고있었지" },
@@ -57,7 +69,7 @@ function removeTalking() {
     { tall: "그래그래" },
   ];
 
-  alert(i);
+  
   --i;
   let a = talks[i];
   const jsona = JSON.stringify(a);
@@ -68,42 +80,27 @@ function removeTalking() {
   newTalk.setAttribute("id", "myTalk");
   newTalk.style.fontSize = "30px";
   newTalk.style.textAlign = "center";
+  let imgpapa = document.getElementById("imgNow");
+
+  let newImg = document.createElement("img");
+  newImg.innerHTML = a.img;
+  newImg.setAttribute("id","myImg");
+  newImg.style.width = "300px";
+  newImg.style.height = "200px";
+  newImg.style.textAlign = "center";
+  newImg.src = a.img;
+  
+
   if (i < 0 || i == 0) {
     a = talks[0];
     i++;
   } else if(i > 0 && i<=talks.length){
+    imgpapa.removeChild(myImg);
     obj.removeChild(myTalk);
+    imgpapa.appendChild(newImg);
     obj.appendChild(newTalk);
   }else{
     return;
   }
 }
 
-// function removeTalking() {
-//   let talks = [
-//     { tall: "안녕!!" },
-//     { tall: "반가워ㅎㅎ" },
-//     { tall: "뭐하고있어?" },
-//     { tall: "나?" },
-//     { tall: "나는 유튜브 보고있었지" },
-//     { tall: "아 진짜?!" },
-//     { tall: "와 대단하다!!" },
-//     { tall: "아니야..." },
-//     { tall: "웅웅" },
-//     { tall: "그냥 하고싶었어" },
-//     { tall: "왜 안돼?" },
-//     { tall: "내맘이지!!" },
-//     { tall: "그래그래" },
-//   ];
-
-//   --i;
-//   let a = talks[i];
-//   const jsona = JSON.stringify(a);
-//   let obj = document.getElementById("now");
-//   let newTalk = document.getElementById("div");
-//   newTalk.innerHTML = a.tall;
-//   newTalk.setAttribute("id", "myTalk");
-//   newTalk.style.fontSize = "30px";
-//   newTalk.style.textAlign = "center";
-//   obj.removeChild(newTalk);
-// }
